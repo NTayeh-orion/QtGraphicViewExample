e1e4c99b602bacc3af93e0d8fb645cfe2052dae1
Naser Tayeh <ntayeh@Orionvtech.com>
2025-09-28 09:43:22 +0300

    enhance the wire class + add hover featuer and selecting of line

--- wire.h
+++ wire.h
@@ -5,12 +5,15 @@
 #include <QGraphicsSceneMouseEvent>
 #include <QMessageBox>
 #include <QPen>
-#include <QVector>
 #include <QPointF>
+#include <QVector>
 class Pin; // forward declaration
 
-class Wire : public QGraphicsPathItem
+class Wire : public QGraphicsObject
 {
+    Q_OBJECT
+    Q_PROPERTY(qreal penWidth READ penWidth WRITE setPenWidth)
+    Q_PROPERTY(qreal glowStrength READ glowStrength WRITE setGlowStrength)
 public:
     Wire(Pin *startPin, QGraphicsItem *parent = nullptr);
 
@@ -18,12 +21,34 @@ public:
     // void updatePosition();
     void updatePath(const QPointF &mousePos = QPointF());
 
+    // Glow animation property
+    qreal glowStrength() const { return m_glowStrength; }
+    void setGlowStrength(qreal value);
+
+    // ---------------- for view -----------------
+    qreal penWidth() const { return m_penWidth; }
+    void setPenWidth(qreal w)
+    {
+        m_penWidth = w;
+        update();
+    }
+    QRectF boundingRect() const override { return m_path.boundingRect(); }
+
     ~Wire(); // destructor
     void disconnectPins();
+    void hoverLeaveEvent(QGraphicsSceneHoverEvent *event);
+    void hoverEnterEvent(QGraphicsSceneHoverEvent *event);
 
+protected:
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem *, QWidget *) override;
+    QVariant itemChange(GraphicsItemChange change, const QVariant &value) override;
 
 private:
     Pin *startPin;
     Pin *endPin = nullptr;
+    qreal m_penWidth = 2;
+    QPainterPath m_path;
+    qreal m_glowStrength = 0.0; // animated value
+    QPen m_pen;
 };
 #endif // WIRE_H
